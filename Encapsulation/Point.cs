0f195e03417ef5e7ac2709c1e234bfae2f3d9b4f
YagurtovAnton <Yagur_oo18@top-academy.ru>
2024-10-12 16:45:28 +0300

    Encapsulation

--- Encapsulation/Point.cs
+++ Encapsulation/Point.cs
@@ -8,58 +8,102 @@ namespace Encapsulation
 {
 	internal class Point
 	{
-		/*double x;
-		double y;
-		
-		public double X
+		//double x;	//smallCamal
+		//double y;
+		//public double X	//Property //BigCamal
+		//{
+		//	get
+		//	{
+		//		return x;
+		//	}
+		//	set
+		//	{
+		//		if (value > 200) value = 200;
+		//		x = value;	//ключевое слово 'value' дает принимаемое значение
+		//	}
+		//}
+		//public double Y
+		//{
+		//	get
+		//	{
+		//		return y;
+		//	}
+		//	set
+		//	{
+		//		if (value > 150) value = 150;
+		//		y = value;
+		//	}
+		//}
+		//public double GetX()
+		//{
+		//	return x;
+		//}
+		//public double GetY()
+		//{
+		//	return y;
+		//}
+		//public void SetX(double x)
+		//{
+		//	if (x > 200) x = 200;
+		//	this.x = x;
+		//}
+		//public void SetY(double y)
+		//{
+		//	if (y > 150) y = 150;
+		//	this.y = y;
+		//}
+		public double X { get; set; }   //Автосвойства
+		public double Y { get; set; }
+		public Point(double x = 0, double y = 0)
 		{
-			get
-			{
-				return x;
-			}
-			set
-			{
-				if (value > 220) value = 220;
-				x = value;
-			}
+			this.X = x;
+			this.Y = y;
+			Console.WriteLine($"Constructor:{this.GetHashCode()}");
 		}
-		public double Y
+		public Point(Point other)
 		{
-			get
-			{
-				return y;
-			}
-			set
-			{
-				if (value > 150) value = 150;
-				y = value;
-			}
+			this.X = other.X;
+			this.Y = other.Y;
+			Console.WriteLine($"CopyConstructor:{GetHashCode()}");
 		}
-		public double GetX()
-			{
-			return x;
-			}
-		public double GetY()
-		 {
-		 return y;
-		 }
-		public void SetX(double x)
+		~Point()
 		{
-			if (x > 200) x = 200;
-			this.x = x;	
+			Console.WriteLine($"Destructor:\t{this.GetHashCode()}");
 		}
-		public void SetY(double y)
+		public static Point operator +(Point left, Point right)
 		{
-			if (y > 150) y = 150;
-			this.y = y;
+			Point res = new Point();
+			res.X = left.X + right.X;
+			res.Y = left.Y + right.Y;
+			return res;
+		}
+		public static Point operator -(Point left, Point right)
+		{
+			return new Point
+				(
+					left.X - right.X,
+					left.Y - right.Y
+				);
+		}
+		public static Point operator ++(Point obj)
+		{
+			obj.X++;
+			obj.Y++;
+			return obj;
+		}
+		public static bool operator ==(Point left, Point right)
+		{
+			return left.X == right.X && left.Y == right.Y;
+		}
+		public static bool operator !=(Point left, Point right)
+		{
+			return !(left == right);
+		}
 
-		}*/
-		public double X {  get; set; }//Автосвойства
-		public double Y { get; set; }//Автосвойства
 		public void Print()
 		{
-			Console.WriteLine($"X = {X}\tY ={Y} ");
-		//	Console.WriteLine($"X = {GetX()}\tY ={GetY()} ");
+			Console.WriteLine($"{GetHashCode()}:\tX = {X}\tY = {Y}");
+			//Console.WriteLine($"X = {GetX()}\tY = {GetY()}");
 		}
 	}
 }
