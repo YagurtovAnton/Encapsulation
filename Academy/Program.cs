36db7c35872afeadd9b9ef1a63c25ee7cecc7b46
YagurtovAnton <Yagur_oo18@top-academy.ru>
2024-10-13 15:03:21 +0300

    Print in Save DONE

--- Academy/Program.cs
+++ Academy/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿#define INHERITANCE_CHECK
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -27,8 +30,31 @@ namespace Academy
 			Human[] group = new Human[]
 				{
 					new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 96),
-					new Teacher("White", "Walter", 50, "Chemistry", 25)
+					new Teacher("White", "Walter", 50, "Chemistry", 25),
+					new Graduate("Schreder", "Hank", 40, "Criminalystic", "DEA", 70, 40, "How to catch Heisenberg"),	//Drug Enforcement Administration
+					new Student("Vercetty", "Tommy", 30, "Theft", "Vice", 97, 98),
+					new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
 				};
+			Print(group);
+			Save(group, "group.txt");
+		}
+		static void Print(Human[] group)
+		{
+			for (int i = 0; i < group.Length; i++)
+			{
+				Console.WriteLine(group[i]);
+			}
+			Console.WriteLine();
+		}
+		static void Save(Human[] group, string filename)
+		{
+			StreamWriter sw = new StreamWriter(filename);
+			for (int i = 0; i < group.Length; i++)
+			{
+				sw.WriteLine(group[i]);
+			}
+			sw.Close();
+			Process.Start("notepad", filename);
 		}
 	}
 }
--- Academy/Student.cs
+++ Academy/Student.cs
@@ -7,36 +7,41 @@ using System.Threading.Tasks;
 
 namespace Academy
 {
-	internal class Student:Human
+	internal class Student : Human
 	{
+		static readonly int SPECIALITY_WIDTH = 24;
+		static readonly int GROUP_WIDTH = 8;
+		static readonly int RATING_WIDTH = 8;
+		static readonly int ATTENDANCE_WIDTH = 8;
 		public string Speciality { get; set; }
-	public string Group { get; set; }
-	public double Rating { get; set; }
-	public double Attendance { get; set; }
-	public Student
-		(
-		string lastName, string firstName, int age,
-		string speciality, string group, double rating, double attendance
-		) : base(lastName, firstName, age)
-	{
-		Speciality = speciality;
-		Group = group;
-		Rating = rating;
-		Attendance = attendance;
-		Console.WriteLine($"SConstructor:{GetHashCode()}");
+		public string Group { get; set; }
+		public double Rating { get; set; }
+		public double Attendance { get; set; }
+		public Student
+			(
+			string lastName, string firstName, int age,
+			string speciality, string group, double rating, double attendance
+			) : base(lastName, firstName, age)
+		{
+			Speciality = speciality;
+			Group = group;
+			Rating = rating;
+			Attendance = attendance;
+			Console.WriteLine($"SConstructor:{GetHashCode()}");
+		}
+		~Student()
+		{
+			Console.WriteLine($"SDestructor:{GetHashCode()}");
+		}
+		public override void Print()
+		{
+			base.Print();
+			Console.WriteLine($"{Speciality} {Group} {Rating} {Attendance}");
+		}
+		public override string ToString()
+		{
+			return base.ToString() +
+				$" {Speciality.PadRight(SPECIALITY_WIDTH)} {Group.PadRight(GROUP_WIDTH)} {Rating.ToString().PadRight(RATING_WIDTH)} {Attendance.ToString().PadRight(ATTENDANCE_WIDTH)}";
+		}
 	}
-	~Student()
-	{
-		Console.WriteLine($"SDestructor:{GetHashCode()}");
-	}
-	public override void Print()
-	{
-		base.Print();
-		Console.WriteLine($"{Speciality} {Group} {Rating} {Attendance}");
-	}
-	public override string ToString()
-	{
-		return base.ToString() + $" {Speciality} {Group} {Rating} {Attendance}";
-	}
-}
 }
